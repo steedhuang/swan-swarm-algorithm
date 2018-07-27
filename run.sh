a86e70aef60ca4f03d97bee81e0ee29e3e567dc7
Jun Steed Huang <steedhuang@ujs.edu.cn>
2018-07-27 21:45:32 +0800

    Add batch file
    This is the batch file runs all of the mains 10 times, to mimic the parallel simulation of 60x7=420 robots all together.

---
+++ run.sh
@@ -0,0 +1,142 @@
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 1
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 2
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 3
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 4
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 5
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 6
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 7
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 8
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 9
+./main1
+sleep 0.618
+./main2
+sleep 0.618
+./main3
+sleep 0.618
+./main4
+sleep 0.618
+./main5
+sleep 0.618
+./main6
+sleep 0.618
+./main7
+sleep 10
+
+
